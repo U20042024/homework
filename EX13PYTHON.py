6b961ea4bf8efd04ac53e38e5e470f0676517b84
Oussama Bouna <147092989+U20042024@users.noreply.github.com>
2023-10-12 02:40:55 +0200

    Add files via upload

---
+++ EX13PYTHON.py
@@ -0,0 +1,11 @@
+m = float(input("enter a number : "))
+n = float(input("enter another number : "))
+if ( m >= 0 and n >= 0 ) or ( m <= 0 and n <= 0) :
+    k = m
+    m= n
+    n = k
+    print("the first number is : " , m)
+    print("the second number is : " , n)
+else :
+    print ("the sum is : " , m + n)
+    print ("and the multiplication is : ", m * n)
